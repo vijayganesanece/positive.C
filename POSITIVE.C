ea26f32726af8b4ccf64dc5795595aa59a942ed0
vijayganesanece <47973535+vijayganesanece@users.noreply.github.com>
2019-02-27 02:38:51 -0800

    Create POSITIVE.C

---
+++ POSITIVE.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter a");
+scanf("%d",&a);
+if(a==0)
+{
+printf("zero");
+}
+if(a>0);
+{
+printf("positive");
+}
+else
+{
+printf("negative");
+}
+return 0;
+}
